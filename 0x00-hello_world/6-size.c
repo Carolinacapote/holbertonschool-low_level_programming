b2dac9d505e138bafcacabf5bb141b62ef3f0b46
Carolinacapote <carolina.capote28@gmail.com>
2021-02-03 23:46:11 +0000

    C that prints size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,12 @@
 /**
  * main - Entry point
  *
+ *char charType
+ *int intType
+ *long int longintType
+ *long long int longlongintType
+ *float floatType
+ *
  * Return: Always 0 (Success)
  */
 
@@ -15,11 +21,11 @@ int main(void)
 	long long int longlongintType;
 	float floatType;
 
-	printf("Size of char: %lu byte\n", sizeof(charType));
-	printf("Size of int: %lu bytes\n", sizeof(intType));
-	printf("Size of long int: %lu bytes\n", sizeof(longintType));
-	printf("Size of long long int: %lu bytes\n", sizeof(longlongintType));
-	printf("Size of float: %lu bytes\n", sizeof(floatType));
+	printf("Size of char: %lu byte", sizeof(charType));
+	printf("Size of int: %lu bytes", sizeof(intType));
+	printf("Size of long int: %lu bytes", sizeof(longintType));
+	printf("Size of long long int: %lu bytes", sizeof(longlongintType));
+	printf("Size of float: %lu bytes", sizeof(floatType));
 
 	return (0);
 }
