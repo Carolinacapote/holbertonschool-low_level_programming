70dc5338b5481f230b3b458bd84290b4c88816f4
Carolinacapote <carolina.capote28@gmail.com>
2021-02-09 16:43:50 +0000

    Printing alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 97 ; ch <= 122 ; ch++)
+	{
+		_putchar(ch);
+	}
+	 _putchar('\n');
+	return (0);
+}
