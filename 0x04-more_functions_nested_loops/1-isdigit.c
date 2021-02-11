03f63706bf69852771827c2caa1e6e25ed86f592
Carolinacapote <carolina.capote28@gmail.com>
2021-02-11 15:31:12 +0000

    Cheking for digits 0 through 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _isdigit - checking for digits.
+ *@c: Characters to be checked
+ *
+ * Return: 1 if c is a digit. 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
