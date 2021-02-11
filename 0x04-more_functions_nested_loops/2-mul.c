f4e3f590b040b2784b8d53196ea22404d2c834d4
Carolinacapote <carolina.capote28@gmail.com>
2021-02-11 15:49:08 +0000

    Multiplying 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - Function that multiplies 2 digits. Start point.
+ *@a: digit to be multiplied
+ *@b: second digit to be multiplied.
+ * Return: result.
+ */
+
+int mul(int a, int b)
+{
+	int result = 0;
+
+	result = (a * b);
+	return (result);
+}
