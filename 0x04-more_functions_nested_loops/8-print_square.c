ffa7bb4337b358137d325ebe70724a2d08435e39
Carolinacapote <carolina.capote28@gmail.com>
2021-02-12 01:41:02 +0000

    Printing a square n size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *print_square - Function that prints a square followed by new line
+ *@size: is the size of the square
+ *
+ * Return: void.
+ */
+
+void print_square(int size)
+{
+	char sharp = '#';
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+			_putchar(sharp);
+		_putchar('\n');
+	}
+}
