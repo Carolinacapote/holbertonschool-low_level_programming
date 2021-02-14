87c73358ea52fe9518b81a69587cd8fd4eee3418
Carolinacapote <carolina.capote28@gmail.com>
2021-02-14 23:22:02 +0000

    Printing numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,33 +6,18 @@
  */
 void print_to_98(int n)
 {
-	int a;
-	int b;
-
-	if (n <= 98)
+	if (n < 98)
 	{
-		for (a = n ; a <= 98 ; a++)
-		{
-			if (a != 98)
-			{
-				printf("%d, ", a);
-			}
-			else
-			{
-				printf("%d\n", a);
-			}
-		}
+		printf("%d, ", n);
+		n++;
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
-		for (b = n ; b >= 98 ; b--)
-		{
-			if (b != 98)
-			{
-				printf("%d, ", b);
-			}
-			else if (b == 98)
-				printf("%d\n", b);
-		}
+		printf("%d, ", n);
+		n--;
+	}
+	else
+	{
+		printf("%d\n", n);
 	}
 }
