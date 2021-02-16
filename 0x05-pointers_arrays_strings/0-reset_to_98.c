389b3aa4d6f20cd36fea45fedd7814fe62db0f3e
Carolinacapote <carolina.capote28@gmail.com>
2021-02-16 01:24:07 +0000

    Take a pointer of an int as parameter and chage value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *reset_to_98 - Function that takes a pointer to an int as parameter and
+ *updates the value it points to to 98.
+ *@n: integer to be used, *n pointer contains adress of n.
+ *
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
