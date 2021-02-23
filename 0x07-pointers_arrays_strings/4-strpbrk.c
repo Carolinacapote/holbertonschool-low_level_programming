e58fdb6e2e59805754df535af86e247978879b94
Carolinacapote <carolina.capote28@gmail.com>
2021-02-23 01:03:00 +0000

    function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,17 +9,15 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	{
-		int i, j = 0;
+	int i, j = 0;
 
-		i = 0;
-		while (s[i])
-		{
-			for (j = 0; accept[j] != 0; j++)
-				if (s[i] == accept[j])
-					return (s + i);
-			i++;
-		}
-		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		for (j = 0; accept[j] != 0; j++)
+			if (s[i] == accept[j])
+				return (s + i);
+		i++;
 	}
+	return (NULL);
 }
