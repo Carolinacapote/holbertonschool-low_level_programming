ab9b33bd11d4ee206812113699cb8e96bb557f0e
Carolinacapote <2621@holbertonschool.com>
2021-02-27 22:10:56 +0000

    program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	if (argc > 1)
+	if (argc > 1 && argc == 3)
 	{
 		for (i = 1; i < argc; i++)
 		{
@@ -24,5 +24,4 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	return (0);
 }
