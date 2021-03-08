bd280fec1bef73ef8df3575148581020aaa0f4a3
Carolinacapote <2621@holbertonschool.com>
2021-03-08 22:04:01 +0000

    New type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- *
- *
- *
+ * struct dog - New structure.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
 	char *name;
 	float age;
-	char *owner
+	char *owner;
 };
+
 typedef struct dog dog;
 #endif
