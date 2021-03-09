ad10619c259e09a846d97ac3fe5163dffc512b53
Carolinacapote <2621@holbertonschool.com>
2021-03-09 00:55:29 +0000

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,56 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strdup_own - Function that returns a pointer to a newly allocated space in
+ * memory, which contains a copy of the string given as a parameter.
+ * @owner: String to be duplicated.
+ * Return: Pointer to the duplicate of str, NULL if str = NULL or if
+ * insufficient memory is available.
+ */
+char *_strdup_own(char *owner)
+{
+	char *dupli_own;
+	int len = 0, i;
+
+	if (owner == NULL)
+		return (NULL);
+	while (owner[len])
+		len++;
+	dupli_own = malloc(sizeof(char) * len + 1);
+	if (!dupli_own)
+		return (NULL);
+	for (i = 0; owner[i]; i++)
+		dupli_own[i] = owner[i];
+	dupli_own[i] = '\0';
+	return (dupli_own);
+	free(dupli_own);
+}
+/**
+ * _strdup - Function that returns a pointer to a newly allocated space in
+ * memory, which contains a copy of the string given as a parameter.
+ * @name: String to be duplicated.
+ * Return: Pointer to the duplicate of str, NULL if str = NULL or if
+ * insufficient memory is available.
+ */
+char *_strdup(char *name)
+{
+	char *dupli_name;
+	int len = 0, i;
+
+	if (name == NULL)
+		return (NULL);
+	while (name[len])
+		len++;
+	dupli_name = malloc(sizeof(char) * len + 1);
+	if (!dupli_name)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		dupli_name[i] = name[i];
+	dupli_name[i] = '\0';
+	return (dupli_name);
+	free(dupli_name);
+}
+/**
  * new_dog - function that creates a new dog.
  * @name: Of the dog.
  * @age: Of the dog.
@@ -15,8 +65,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_doggy = malloc(sizeof(dog_t));
 	if (!new_doggy)
 		return (NULL);
-	new_doggy->name = name;
+	new_doggy->name = _strdup(name);
 	new_doggy->age = age;
-	new_doggy->owner = owner;
+	new_doggy->owner = _strdup_own(owner);
 	return (new_doggy);
 }
