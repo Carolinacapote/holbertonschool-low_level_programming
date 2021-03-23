fc24e79d120cec16e3deac748856fa789158afe2
Carolinacapote <2621@holbertonschool.com>
2021-03-23 17:34:46 +0000

    Frees a linked list and sets head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
-	listint_t *tmp_2;
 
-	if (*head != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		tmp = *head;
-		while (tmp != NULL)
-		{
-			tmp_2 = tmp;
-			tmp = tmp->next;
-			free(tmp_2);
-		}
-		*head = NULL;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
+	*head = NULL;
 }
