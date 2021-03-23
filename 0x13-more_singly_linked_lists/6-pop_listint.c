90ccc7799401d151cb8c29d3e5b2f287290ffb64
Carolinacapote <2621@holbertonschool.com>
2021-03-23 17:12:56 +0000

    Deleting the head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,12 +7,12 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
-	int i = 0;
+	listint_t *delete;
 
 	if (*head == NULL)
 		return (0);
 	tmp = *head;
-	if (tmp != NULL && tmp->n == i)
-		*head = tmp->next;
-	return (tmp->n);
+	delete = tmp;
+	*head = tmp->next;
+	return (delete->n);
 }
