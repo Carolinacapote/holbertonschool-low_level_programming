a66bf009e6caa201d8386cffa8819bb3209c1afa
Carolinacapote <2621@holbertonschool.com>
2021-03-23 22:06:52 +0000

    deleting the head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,11 +7,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
+	int number;
 
 	if (*head == NULL)
 		return (0);
+	number = (*head)->n;
 	tmp = *head;
 	*head = (*head)->next;
 	free(tmp);
-	return (tmp->n);
+	return (number);
 }
