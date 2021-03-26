30cd728dd687d85535b60607e59031f899b3ed3e
Carolinacapote <2621@holbertonschool.com>
2021-03-26 01:58:15 +0000

    Set the bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * set_bit - function that returns the value of a bit at a given index.
+ * @n: decimal number to be used.
+ * @index: position of the digit in the binary representation.
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int i;
+	unsigned int a[1024];
+	unsigned int number = *n;
+
+	if (!number)
+		return (-1);
+	if (number < 1)
+	{
+		number = 1;
+	}
+	for (i = 0; number > 0; i++)
+	{
+		a[i] = number % 2;
+		number = number / 2;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		if (i == (int)index)
+			a[i] = 1;
+		i--;
+	}
+	return ((int) a[i]);
+}
