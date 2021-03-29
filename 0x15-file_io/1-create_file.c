9b651664f5902f046a155a3f938e6ad017a5c20e
Carolinacapote <2621@holbertonschool.com>
2021-03-29 23:03:10 +0000

    Creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * create_file - function that creates a file.
+ * @filename: Name of the file to be created.
+ * @text_content: Text to be writen in the new file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int new_file;
+	int i = 0;
+
+	if (filename == NULL)
+		return (-1);
+	new_file = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (!new_file)
+		return (-1);
+        while (text_content[i] != '\0')
+	{
+		i++;
+	}
+	write(new_file, text_content, i);
+	close (new_file);
+	return (1);
+}
