e1a5d914f43467f337dc43c99cb2e2c2ffccc5b8
Carolinacapote <2621@holbertonschool.com>
2021-03-29 23:10:31 +0000

    Creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	new_file = open(filename, O_CREAT | O_WRONLY, 0600);
-	if (!new_file)
+	if (new_file < 0)
 		return (-1);
 	while (text_content[i] != '\0')
 	{
