f0dbebe8aa057007050670e04abb315c2985cf9b
Carolinacapote <2621@holbertonschool.com>
2021-03-30 01:07:51 +0000

    Reading 'n' chars of a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+/**
+ * read_textfile - function that reads a text file and prints it.
+ * @filename: Name of the file to be created.
+ * @letters: number of letters to be printed.
+ * Return: number of letters.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int text = 0, to_read;
+	char *buf;
+
+	buf = malloc(letters * sizeof(char));
+	if (filename == NULL)
+		return (0);
+	text = open(filename, O_RDONLY);
+	if (text == -1)
+		return (0);
+	to_read = read(text, buf, letters);
+	if (to_read == -1)
+		return (0);
+	write(STDOUT_FILENO, buf, to_read);
+	close(text);
+	return (to_read);
+}
