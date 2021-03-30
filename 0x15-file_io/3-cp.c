4419ea7891afa3498db39c372713718b53502bff
Carolinacapote <2621@holbertonschool.com>
2021-03-30 03:12:50 +0000

    pr

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,23 +3,23 @@
 #include <unistd.h>
 #include <stlib.h>
 /**
- * main -
- * @argc:
+ * main - functionto copy a file.
+ * @argc: number of parameters.
+ * @argv: text in the positions.
  */
 void main(int argc, char *argv[])
 {
-	char *error_msg2 = "Error: Can't read from file ";
 	char *file_from[1024] = argv[1], *file_to = argv[2];
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	if (!argv[1])
 	{
-		write(STDERR_FILENO, error_msg2, 28)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[0])
 		exit(98);
 	}
 }
