f95236c1593833e1267829a00b7cd5fe3b8f4593
Carolinacapote <carolina.capote28@gmail.com>
2021-05-02 20:48:04 +0000

    test

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -22,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (delete->next != NULL)
+	while (delete != NULL)
 	{
 		prev_node = delete;
 		delete = delete->next;
