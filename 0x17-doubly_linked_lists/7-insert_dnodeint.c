176a7e2785d10d036ea672d19acc2d4bea717821
Carolinacapote <carolina.capote28@gmail.com>
2021-05-02 20:18:04 +0000

    test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,13 +17,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
-	if (*h == NULL)
-	{
-		new_node->next = *h;
-		*h = new_node;
-		new_node->next = NULL;
-		new_node->prev = NULL;
-	}
 	if (idx == 0)
 	{
 		new_node->next = *h;
