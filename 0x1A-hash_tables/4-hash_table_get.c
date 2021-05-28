fc96f23b2d2facd9bbb83b61fd2d12eb80fe34a0
Carolinacapote <carolina.capote28@gmail.com>
2021-05-28 15:54:51 +0000

    Returning value of a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that retrieves a value associated with a key.
+ * @ht:  is the hash table you want to look into.
+ * @key: is the key you are looking for.
+ * Return: the value of the key or NULL if the key is not found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long int index;
+
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+
+		current = current->next;
+	}
+
+	return (NULL);
+}
