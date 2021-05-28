2ec4496d756270c0ac75a10fb161335eafc08c50
Carolinacapote <carolina.capote28@gmail.com>
2021-05-28 14:54:42 +0000

    Creating a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,11 +9,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 
 	if (size == 0)
-		size++;
+		return (NULL);
 
-	hash_table = malloc(sizeof(hash_table_t) * size);
+	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 		return (NULL);
+
+	hash_table->array = malloc(sizeof(hash_table_t *) * size);
+	if (!hash_table->array)
+		return (NULL);
+
+	hash_table->size = size;
 
 	return (hash_table);
 }
