917480ba21563b986bb707e6c8e76931147ed92d
Carolinacapote <carolina.capote28@gmail.com>
2021-05-28 15:57:02 +0000

    Returning value of a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *current;
 	unsigned long int index;
 
+	if (!key || !ht)
+		return (NULL);
+
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
 
