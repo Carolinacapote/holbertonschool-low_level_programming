83aa46633b945c05a9c035d187cbc192b4e99fbc
Carolinacapote <carolina.capote28@gmail.com>
2021-07-04 19:39:08 +0000

    Returning the perimeter of an island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains a function called island_perimeter()
+"""
+
+
+def island_perimeter(grid):
+    """
+    Attributes:
+        grid (list): List of integers. 0 is water zone, 1 is land zone
+    Returns:
+        perimeter (int):The perimeter of the island (grid)
+    """
+
+    perimeter = 0
+    height = len(grid)
+
+    for i in range(height):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
